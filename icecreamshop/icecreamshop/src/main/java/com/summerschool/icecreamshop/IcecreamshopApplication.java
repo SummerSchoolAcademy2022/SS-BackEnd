570aebf4b753a0fe55b47259fee19969458ea171
Ioana Trandafir <itrandafir@deloittece.com>
2022-08-02 09:06:12 +0300

    Add BE project

---
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/IcecreamshopApplication.java
@@ -0,0 +1,13 @@
+package com.summerschool.icecreamshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcecreamshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcecreamshopApplication.class, args);
+	}
+
+}
---
+++ icecreamshop/icecreamshop/src/test/java/com/summerschool/icecreamshop/IcecreamshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summerschool.icecreamshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcecreamshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

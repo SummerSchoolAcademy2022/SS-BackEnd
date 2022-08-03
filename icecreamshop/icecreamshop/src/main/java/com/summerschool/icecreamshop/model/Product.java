8ca05de6acdb562a060a762382824f7472ea2d26
Alexandru Pavel <alex.pavel2002@gmail.com>
2022-08-03 12:23:54 +0300

    product entity setId and getId methods

--- icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/model/Product.java
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/model/Product.java
@@ -13,4 +13,12 @@ public class Product {
 
     public Product() {
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }

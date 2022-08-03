0d34b773c109e16ea20fbe0903a98dc04202bf48
Alexandru Pavel <alex.pavel2002@gmail.com>
2022-08-03 22:20:51 +0300

    Moved root url mapping /proucts to class level

--- icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/controller/ProductController.java
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/controller/ProductController.java
@@ -6,21 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
     ProductService productService;
 
-    @GetMapping("/products/{productId}")
-    ResponseEntity<Optional<Product> > readById(@PathVariable Integer productId) {
-        Optional<Product> product = productService.findById(productId);
-        if(product.isPresent())
-            return new ResponseEntity<>(product, HttpStatus.OK);
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping("/{productId}")
+    public ResponseEntity<Product> readById(@PathVariable Integer productId) {
+        Optional<Product> optionalProduct = productService.findById(productId);
+        return optionalProduct.map(product -> new ResponseEntity<>(product, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }

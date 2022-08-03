1cd65c6b9d5e27e2aa9ff9dc166d92078f01aac0
Titrat Cristina <titratcristina@gmail.com>
2022-08-03 17:00:58 +0300

    Init for database

---
+++ icecreamshop/icecreamshop/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Product (id, title, shortDescription, longDescription, ingredients, quantity, alergens, price, currency,
+                       photoUrl, productType, categoryId)
+VALUES (1, 'Pistachio Ice Cream', 'Good ice cream', 'Treat yourself to ice cream infused with smooth Bourbon', 'A', 3, 'A,B,C',
+        10, );

81241f9cf7b9d436f9ff51e0169e6febab43bb32
ilorrrena <lori_lorena2010@yahoo.com>
2022-08-03 16:59:46 +0300

    Changed repositories from class to interface

--- icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/repository/ProductRepository.java
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/repository/ProductRepository.java
@@ -1,4 +1,8 @@
 package com.summerschool.icecreamshop.repository;
 
-public class ProductRepository {
+import com.summerschool.icecreamshop.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends JpaRepository <Integer, Product>{
 }

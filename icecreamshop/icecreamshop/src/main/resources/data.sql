4f4a9cef9d945e7827b9021ed4ef2bc35d7e9ea6
Titrat Cristina <titratcristina@gmail.com>
2022-08-04 16:29:26 +0300

    Update data

--- icecreamshop/icecreamshop/src/main/resources/data.sql
+++ icecreamshop/icecreamshop/src/main/resources/data.sql
@@ -1,17 +1,4 @@
--- DROP TABLE IF EXISTS test;
--- CREATE TABLE test
--- (
---     id    INT AUTO_INCREMENT PRIMARY KEY,
---     input VARCHAR(250) NOT NULL
--- );
---
--- INSERT INTO test (input)
--- VALUES ('This is a test input');
---
--- SELECT * FROM STUDENT;
---
--- INSERT INTO STUDENT (id, name, age, email)
--- VALUES (1, 'a', 1, 'a');
+
 
 INSERT INTO CATEGORY (id, name, description)
 VALUES (1, 'ICE_CREAM', 'Delicious ice cream.');

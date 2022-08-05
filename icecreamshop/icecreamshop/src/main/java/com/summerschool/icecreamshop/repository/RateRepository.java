f8260cd685f1547aad44ad15418b8af5448d892e
Titrat Cristina <titratcristina@gmail.com>
2022-08-05 17:02:50 +0300

    Fixed bugs to run app

--- icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/repository/RateRepository.java
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/repository/RateRepository.java
@@ -1,8 +1,9 @@
 package com.summerschool.icecreamshop.repository;
 
+import com.summerschool.icecreamshop.model.Rate;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RateRepository extends CrudRepository<RateRepository, Integer> {
+public interface RateRepository extends CrudRepository<Rate, Integer> {
 }
--- icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/repository/StudentRepository.java
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/repository/StudentRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends CrudRepository<Student, Integer>
-{
+public interface StudentRepository extends CrudRepository<Student, Integer> {
 }

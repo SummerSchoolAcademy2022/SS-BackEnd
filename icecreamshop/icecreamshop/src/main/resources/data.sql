2f7a0b340b414b67408ebb635cf7fe0e1fc6a059
roxanaionita <roxana.i71@yahoo.com>
2022-08-05 03:04:06 -0700

    Added rate

--- icecreamshop/icecreamshop/src/main/resources/data.sql
+++ icecreamshop/icecreamshop/src/main/resources/data.sql
@@ -11,3 +11,54 @@ VALUES (3, 'DONUTS', 'Delicious donuts!');
 
 INSERT INTO CATEGORY (id, name, description)
 VALUES (4, 'MERCHANDISE', 'Cool merch.');
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (1, 1, 5);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (2, 1, 1);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (3, 2, 4);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (4, 2, 3);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (5, 3, 2);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (6, 3, 3);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (7, 4, 2);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (8, 4, 5);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (9, 5, 3);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (10, 5, 4);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (11, 6, 3);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (12, 6, 5);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (13, 7, 4);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (14, 7, 2);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (15, 8, 5);
+
+INSERT INTO RATE(id,  productId, rate)
+VALUES (16, 8, 5);
+
+
+

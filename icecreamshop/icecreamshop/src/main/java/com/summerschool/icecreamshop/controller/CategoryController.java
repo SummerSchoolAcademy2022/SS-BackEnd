46aed605ec1b800b830ecdf56c2fb98703d59fe3
elRaluca <elenaralucalorena@gmai.com>
2022-08-08 17:22:22 +0300

    Removed testing methods

--- icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/controller/CategoryController.java
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/controller/CategoryController.java
@@ -1,39 +1,4 @@
 package com.summerschool.icecreamshop.controller;
-import java.util.List;
-import com.summerschool.icecreamshop.model.Category;
-import com.summerschool.icecreamshop.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-
-@RestController
 public class CategoryController {
-    //autowired the StudentService class
-    @Autowired
-    CategoryService categoryService;
 
-    //creating a get mapping that retrieves all the students detail from the database
-    @GetMapping("/category")
-    private List<Category> getAllCategory() {
-        return categoryService.getAllCategory();
-    }
-
-    //creating a get mapping that retrieves the detail of a specific student
-    @GetMapping("/category/{id}")
-    private Category getCategory(@PathVariable("id") Long id) {
-        return categoryService.getCategoryById(id);
-    }
-
-    //creating a delete mapping that deletes a specific student
-    @DeleteMapping("/category/{id}")
-    private void deleteCategory(@PathVariable("id") Long id) {
-        categoryService.delete(id);
-    }
-
-    //creating post mapping that post the student detail in the database
-    @PostMapping("/category")
-    private int saveCategory(@RequestBody Category category) {
-        categoryService.saveOrUpdate(category);
-        return (int) category.getId();
-    }
 }
--- icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/service/CategoryService.java
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/service/CategoryService.java
@@ -1,38 +1,6 @@
 package com.summerschool.icecreamshop.service;
-
-import com.summerschool.icecreamshop.model.Category;
-import com.summerschool.icecreamshop.model.Student;
-import com.summerschool.icecreamshop.repository.CategoryRepository;
-import com.summerschool.icecreamshop.repository.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 public class CategoryService {
-    @Autowired
-    CategoryRepository categoryRepository;
 
-    //getting all student records
-    public List<Category> getAllCategory() {
-        List<Category> categories = new ArrayList<Category>();
-        categoryRepository.findAll().forEach(category -> categories.add(category));
-        return categories;
-    }
-
-    //getting a specific record
-    public Category getCategoryById(Long id) {
-        return categoryRepository.findById(id).get();
-    }
-
-    public void saveOrUpdate(Category category) {
-        categoryRepository.save(category);
-    }
-
-    //deleting a specific record
-    public void delete(Long id) {
-        categoryRepository.deleteById(id);
-    }
 }

1ef4ad06009399b70f334de2cceaefaeb728e569
elRaluca <elenaralucalorena@gmai.com>
2022-08-08 17:22:44 +0300

    Removed test class

--- icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.summerschool.icecreamshop.repository;
-
-import com.summerschool.icecreamshop.model.Student;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends CrudRepository<Student, Integer> {
-}

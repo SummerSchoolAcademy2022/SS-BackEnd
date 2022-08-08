0664675d2743fece6a6a3b46fa4e902ca6acf1c2
Titrat Cristina <titratcristina@gmail.com>
2022-08-08 15:42:37 +0300

    Add BasketProduct class

---
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/model/BasketProduct.java
@@ -0,0 +1,60 @@
+package com.summerschool.icecreamshop.model;
+
+import javax.persistence.*;
+
+@Entity
+public class BasketProduct {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Double price;
+    private Integer quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.EAGER,optional = false)
+    @JoinColumn(name = "basket_id", nullable = false)
+    private Basket basket;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Basket getBasket() {
+        return basket;
+    }
+
+    public void setBasket(Basket basket) {
+        this.basket = basket;
+    }
+}

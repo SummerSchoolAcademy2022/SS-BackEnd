67c7e2e08d0f6bc644dc508b8e9b70e8cf50e266
ilorrrena <lori_lorena2010@yahoo.com>
2022-08-09 14:58:24 +0300

    Add dto for category

--- icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/dto/CategoryDTO.java
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/dto/CategoryDTO.java
@@ -1,4 +1,61 @@
 package com.summerschool.icecreamshop.dto;
 
+import com.sun.istack.NotNull;
+
+import java.util.List;
+
 public class CategoryDTO {
-}
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    private String description;
+
+    private List<ProductDTO> products;
+
+
+    public CategoryDTO() {
+    }
+
+    public CategoryDTO(Long id, String name, String description, List<ProductDTO> products) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<ProductDTO> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductDTO> products) {
+        this.products = products;
+    }
+
+    }
+

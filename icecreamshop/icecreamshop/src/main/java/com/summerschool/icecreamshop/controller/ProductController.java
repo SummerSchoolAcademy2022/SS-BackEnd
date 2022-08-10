e985432d1a7c535dda92c1125a864f1d024a65de
Ana-Maria <anamariapuscasu@yahoo.com>
2022-08-10 17:14:10 +0300

    CREATE PRODUCT

--- icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/controller/ProductController.java
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/controller/ProductController.java
@@ -4,9 +4,11 @@ package com.summerschool.icecreamshop.controller;
 
 import com.summerschool.icecreamshop.dto.ProductDTO;
 
+import com.summerschool.icecreamshop.model.Product;
 import com.summerschool.icecreamshop.service.ProductService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
@@ -38,4 +40,13 @@ public class ProductController {
                 .map(product -> modelMapper.map(product, ProductDTO.class))
                 .collect(Collectors.toList()));
     }
-}
+
+    @PostMapping()
+    public ResponseEntity<?> create(@RequestBody Product product) {
+        try {
+            product = productService.createProduct(product);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Can't create the produt");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
+}}
--- icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/service/ProductService.java
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/service/ProductService.java
@@ -1,11 +1,13 @@
 package com.summerschool.icecreamshop.service;
 
+import com.summerschool.icecreamshop.exceptions.ResourceNotFoundException;
 import com.summerschool.icecreamshop.model.Product;
 import com.summerschool.icecreamshop.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -20,5 +22,34 @@ public class ProductService {
     public List<Product> getAll() {
         return productRepository.findAll();
     }
+    public Product createProduct(Product product) {
+        if (product.getId() == null) {
+            product = productRepository.save(product);
+            return product;
+        } else {
+            Optional<Product> productOptional = productRepository.findById(productRepository.getId());
+
+            if (productOptional.isPresent()) {
+                Product newProduct = productOptional.get();
+                newProduct.setTitle(newProduct.getTitle());
+                newProduct.setIngredients(newProduct.getIngredients());
+                newProduct.setAlergens(newProduct.getAlergens());
+                newProduct.setCurrency(newProduct.getCurrency());
+                newProduct.setShortDescription(newProduct.getShortDescription());
+                newProduct.setLongDescription(newProduct.getLongDescription());
+                newProduct.setPhotoUrl(newProduct.getPhotoUrl());
+                newProduct.setPrice(newProduct.getPrice());
+                newProduct.setType(newProduct.getType());
+                newProduct.setQuantity(newProduct.getQuantity());
+                newProduct.setBasketProduct(newProduct.getBasketProduct());
+                newProduct.setRate(newProduct.getRate());
+                newProduct = productRepository.save(newProduct);
+                return newProduct;
+            } else {
+                throw new ResourceNotFoundException("No employee record exist for given id");
+            }
+        }
+    }
+
 
 }

0ca3a04323b0c38e250eaa36bb8b9f68227f8211
Alexandru Pavel <alex.pavel2002@gmail.com>
2022-08-10 13:06:46 +0300

    Preluare ProductDTO

---
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/dto/ProductDTO.java
@@ -0,0 +1,126 @@
+package com.summerschool.icecreamshop.dto;
+
+import com.summerschool.icecreamshop.model.ProductType;
+
+
+public class ProductDTO {
+    private Long id;
+    private String title;
+    private String shortDescription;
+    private String longDescription;
+    private String ingredients;
+    private int quantity;
+    private String alergens;
+    private double price;
+    private String currency;
+    private String photoUrl;
+    private ProductType type;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String title, String shortDescription,
+                      String longDescription, String ingredients,
+                      int quantity, String alergens, double price,
+                      String currency, String photoUrl, ProductType type) {
+        this.id = id;
+        this.title = title;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+        this.ingredients = ingredients;
+        this.quantity = quantity;
+        this.alergens = alergens;
+        this.price = price;
+        this.currency = currency;
+        this.photoUrl = photoUrl;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getAlergens() {
+        return alergens;
+    }
+
+    public void setAlergens(String alergens) {
+        this.alergens = alergens;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public ProductType getType() {
+        return type;
+    }
+
+    public void setType(ProductType type) {
+        this.type = type;
+    }
+}

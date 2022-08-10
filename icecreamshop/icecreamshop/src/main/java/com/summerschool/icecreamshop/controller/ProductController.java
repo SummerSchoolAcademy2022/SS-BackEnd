f1700c1d5e78ce2c07e6c3fbca3823fbc2c5a3ce
Alexandru Pavel <alex.pavel2002@gmail.com>
2022-08-10 14:13:30 +0300

    adaugare findall in controller

--- icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/controller/ProductController.java
+++ icecreamshop/icecreamshop/src/main/java/com/summerschool/icecreamshop/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.summerschool.icecreamshop.controller;
 import com.summerschool.icecreamshop.dto.ProductDTO;
 import com.summerschool.icecreamshop.model.Product;
 import com.summerschool.icecreamshop.service.ProductService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/products")
@@ -19,7 +22,15 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+    private ModelMapper modelMapper;
 
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getProducts() {
+        return ResponseEntity.ok(productService.getAll()
+                .stream()
+                .map(product -> modelMapper.map(product, ProductDTO.class))
+                .collect(Collectors.toList()));
+    }
 
     @GetMapping("/{productId}")
     public ResponseEntity<ProductDTO> readById(@PathVariable("productId") Long productId) {
